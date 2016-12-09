a0d631561e67a73aa9eb84212adfa3f99f2f7057
yj.w <yingjiemeow@gmail.com>
2016-12-09 10:57:54 +0800

    cutString

--- src/main/java/com/tools/utils/CutStringUtils.java
+++ src/main/java/com/tools/utils/CutStringUtils.java
@@ -1,6 +1,6 @@
 package com.tools.utils;
 
-import com.yeepay.g3.utils.common.StringUtils;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.UnsupportedEncodingException;
 
@@ -11,6 +11,7 @@ import java.io.UnsupportedEncodingException;
  * @since 16/7/12 下午3:34
  */
 public class CutStringUtils {
+
     private static final String DEFAULT_CHARSET = "UTF-8";
 
     /**
@@ -19,15 +20,18 @@ public class CutStringUtils {
      * @param offset
      * @return
      */
-    public static String subString(String text, int offset) throws UnsupportedEncodingException {
+    public static String subStringByByte(String text, int offset, String charset) throws UnsupportedEncodingException {
         if(StringUtils.isBlank(text) || offset <= 0 || offset > text.length()) {
             return text;
+        }
+        if (StringUtils.isBlank(charset)) {
+            charset = DEFAULT_CHARSET;
         }
         int counter = 0;
         StringBuffer stringBuffer = new StringBuffer();
         int charLength = 0;
         for(int i = 0; i< text.length(); i++) {
-            charLength = getCharLength(text.charAt(i));
+            charLength = getCharLength(text.charAt(i), charset);
             counter = counter + charLength;
             if(counter > offset) {
                 break;
@@ -37,14 +41,16 @@ public class CutStringUtils {
         return stringBuffer.toString();
     }
 
-    public static int getCharLength(char c) throws UnsupportedEncodingException{
-        return String.valueOf(c).getBytes(DEFAULT_CHARSET).length;
+    public static int getCharLength(char c, String charset) throws UnsupportedEncodingException{
+        return String.valueOf(c).getBytes(charset).length;
     }
 
     public static void main(String[] args) throws Exception{
         String text = "测试12345";
         int offset = 4;
-        System.out.println(subString(text, offset));
+        String charset = System.getProperty("file.encoding");
+        System.out.println(charset);
+        System.out.println(subStringByByte(text, offset, null));
         System.out.println(text.substring(0, 7));
     }
 }
